83db7b88ed85f662abe6bfeefd3b78218337cf74
dagmfre <dagmfre@gmail.com>
2024-04-15 11:37:52 +0300

    Fetch bestArtistsIds from deezer API using axios

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,39 @@
+import { useState, useEffect } from "react";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
+import axios from "axios";
 
 export default function Home() {
+  const [bestArtistsIds, setBestArtistsIds] = useState([]);
+  const [bestArtistsTracks, setBestArtistsTracks] = useState([]);
+
+  useEffect(() => {
+    const fetchArtistsIds = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.deezer.com/genre/0/artists"
+        );
+        response.data.map((artistData) => setBestArtistsIds(artistData.id));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchArtistsIds();
+  }, []);
+  console.log(bestArtistsIds);
   return (
     <div className="home">
-      <Header />
+      <Sidebar />
+      <div className="home-main">
+        <Header />
+        <div className="best-album">
+          <img src="" alt="" className="best-album-image" />
+          <div className="best-album-track-cont">
+            {/* https://api.deezer.com/genre/0/artists */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
