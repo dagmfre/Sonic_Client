600757344dc8fb6e44cef8f7678869ca56b70a7d
dagmfre <dagmfre@gmail.com>
2024-05-20 17:19:55 +0300

    Build a component that displays popular artists

---
+++ src/Components/Home/Artist.jsx
@@ -0,0 +1,79 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useState, useEffect } from "react";
+import ArtistLoader from "../ArtistLoader";
+import axios from "axios";
+
+export default function Artist() {
+  const [artists, setArtists] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const artistsCont = css`
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
+    gap: 2rem;
+    padding-top: 5rem;
+    position: relative;
+    h1 {
+      position: absolute;
+      top: 0;
+      font-size: 2rem;
+    }
+    div {
+      display: flex;
+      flex-direction: column-reverse;
+      align-items: center;
+    }
+  `;
+
+  const artistImg = css`
+    border-radius: 50%;
+    width: 100%;
+  `;
+
+  const loaderCont = css`
+    padding: 0 2rem;
+    svg {
+      width: 100%;
+      height: 100%;
+    }
+  `;
+
+  useEffect(() => {
+    const fetchArtists = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/api/artists");
+        setArtists(response.data.reverse());
+        setIsLoading(false)
+      } catch (error) {
+        console.error("Error fetching artists:", error);
+      }
+    };
+
+    fetchArtists();
+  }, []);
+
+  return (
+    <div id="artists">
+      {isLoading ? (
+        <div css={loaderCont}>
+          <ArtistLoader />
+        </div>
+      ) : (
+        <div css={artistsCont}>
+          <h1>Top Artists</h1>
+          {artists.map((artist) => (
+            <div css={artist} key={artist.id}>
+              <h2>{artist.name}</h2>
+              <img
+                css={artistImg}
+                src={artist.picture_medium}
+                alt={artist.name}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
