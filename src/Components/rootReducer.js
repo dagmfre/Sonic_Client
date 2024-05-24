85c6960ca7710e843bb2720e36ac8060dfd91065
dagmfre <dagmfre@gmail.com>
2024-05-24 11:52:48 +0300

    update rootReducer.js by adding topAlbumReducer

--- src/Components/rootReducer.js
+++ src/Components/rootReducer.js
@@ -3,11 +3,13 @@ import currentPlayingReducer from "./currentPlayingSlice";
 import favoriteListReducer from "./favoriteListSlice";
 import isMenuClickedReducer from "./menuClickSlice";
 import artistSlice from "./artistSlice";
+import topAlbumReducer from "./topAlbumSlice";
 
 const rootReducer = combineReducers({
   currentPlayingSong: currentPlayingReducer,
   favoriteList: favoriteListReducer,
   menuClickedStatus: isMenuClickedReducer,
+  topAlbum: topAlbumReducer,
   artists: artistSlice,
 });
 
