25cdde7e42f9709a97756f5ddd4d7bb12ad25beb
dagmfre <dagmfre@gmail.com>
2024-05-31 22:48:12 +0300

    Add userImageReducer to rootReducer

--- src/Components/rootReducer.js
+++ src/Components/rootReducer.js
@@ -2,19 +2,21 @@ import { combineReducers } from "@reduxjs/toolkit";
 import currentPlayingReducer from "./currentPlayingSlice";
 import favoriteListReducer from "./favoriteListSlice";
 import isMenuClickedReducer from "./menuClickSlice";
-import artistSlice from "./artistSlice";
+import artistReducer from "./artistSlice";
 import topAlbumReducer from "./topAlbumSlice";
 import tracksReducer from "./tracksSlice";
-import userSongSlice from "./userSongSlice";
+import userSongReducer from "./userSongSlice";
+import userImageReducer from "./userImageSlice";
 
 const rootReducer = combineReducers({
   currentPlayingSong: currentPlayingReducer,
   favoriteList: favoriteListReducer,
   menuClickedStatus: isMenuClickedReducer,
   topAlbum: topAlbumReducer,
-  artists: artistSlice,
+  artists: artistReducer,
   tracks: tracksReducer,
-  userSong: userSongSlice,
+  userSong: userSongReducer,
+  userImages: userImageReducer
 });
 
 export default rootReducer;
