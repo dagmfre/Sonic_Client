234c1f478e491aef254570bd722734ac147924b6
dagmfre <dagmfre@gmail.com>
2024-05-31 23:04:04 +0300

    Create saga for handling userImages

---
+++ src/Components/userImageSaga.js
@@ -0,0 +1,32 @@
+import { all, call, put, takeLatest } from "redux-saga/effects";
+import {
+  fetchUserImagesSuccess,
+  fetchUserImagesFailure,
+} from "./userImageSlice";
+// import axios from "axios";
+
+function* fetchUserImage(myList) {
+  const response = yield call(fetch, `http://localhost:3001/file/${myList.imageFileName}`);
+  const blob = yield call([response, 'blob']); // call method on response object
+  return URL.createObjectURL(blob);
+}
+
+function* fetchUserImages(action) {
+  try {
+    const data = action.payload;
+    const imageSrcs = yield all(data.map((myList) => call(fetchUserImage, myList)));
+
+    const updatedLists = data.map((myList, index) => ({
+      ...myList,
+      imgSrc: imageSrcs[index],
+    }));
+
+    yield put(fetchUserImagesSuccess(updatedLists));
+  } catch (error) {
+    yield put(fetchUserImagesFailure(error.message));
+  }
+}
+
+export function* watchUserImages() {
+  yield takeLatest("userImages/fetchUserImagesRequest", fetchUserImages);
+}
