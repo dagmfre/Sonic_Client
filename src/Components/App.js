fa9ead0045310f968488ed8c8b2326aed3deaf48
dagmfre <dagmfre@gmail.com>
2024-12-23 20:14:33 +0300

    Add registration route to the application

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,7 @@ import Home from "./Home/Home";
 import { Global, css } from "@emotion/react";
 import MyFavorite from "./MyFavorite";
 import Uploader from "./Uploader";
+import Register from "./Register";
 
 const globalStyles = css`
   body {
@@ -24,6 +25,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<MyFavorite />} />
         <Route path="/upload" element={<Uploader />} />
+        <Route path="/reg" element={<Register />} />
       </Routes>
     </>
   );
